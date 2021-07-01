ac83f8a1acfb8e13717d46a0caabf5bb4839ffb7
BigMacRaphi2 <86768897+BigMacRaphi2@users.noreply.github.com>
2021-07-01 14:53:23 +0200

    Create menu-navigation.php

---
+++ menu-navigation.php
@@ -0,0 +1,47 @@
+<nav class="menu-nav">
+<div id="content">
+	<div class="top">
+	<style>
+		div#content div.top{
+			background-color: #007AF4;
+			padding: 15px;
+		}  
+	</style>
+	<ul class="menu">
+		<style class="menu" type="text/css">
+			nav.menu-nav ul li.btn {
+	list-style-type: none;
+	display: inline-block;
+}
+nav.menu-nav ul li.btn a{
+	color: white;
+	background-color: #0473E2;
+	text-decoration: none;
+	padding: 25px;
+}
+nav.menu-nav ul li.btn:hover a{
+	color: white;
+	background-color: #01A1FE;
+	transition: 0,3s;
+}
+		</style>
+		<ul>
+			<li class="btn">
+				<a href="template.html">
+				<b>Accueil</b>
+				</a>
+			</li>
+			<li class="btn">
+				<a href="quisommesnous.html">
+				<b>Qui sommes-nous?</b>
+				</a>
+			</li>
+			<li class="btn">
+				<a href="contact.html">
+				<b>Contact</b>
+				</a>
+			</li>
+		</ul>
+	</ul>
+	</div>
+</div>
